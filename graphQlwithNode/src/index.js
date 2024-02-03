f78c36db61961091a6c86d8d307e43faef11658e
Subham Deb <subham.deb.1253@gmail.com>
2024-02-03 23:40:19 +0530

    initial commit

---
+++ graphQlwithNode/src/index.js
@@ -0,0 +1,33 @@
+import { GraphQLServer } from "graphql-yoga"
+
+
+// type defination Schema
+const typeDefs = `
+    type Query{
+        hello: String!,
+        sayMyName: String!
+    }
+`
+// *** We use ! after type (ex- hello: String!) to make sure return never be null, always string
+// Resolvers
+
+const resolvers = {
+    Query : {
+        hello(){
+            return null
+        },
+        sayMyName(){
+            return "My name is Subham"
+        } 
+    }
+}
+
+
+const server = new GraphQLServer({
+        typeDefs,
+        resolvers
+    })
+
+server.start((e)=>{
+    console.log(`server started at: ${e}`)
+})
