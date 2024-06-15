a61bc6ada17b892913f1865c4eb325be2662301c
Subham Deb <subham.deb.1253@gmail.com>
2024-06-15 16:23:58 +0530

    commit

--- graphQL_Basic/src/index.js
+++ graphQL_Basic/src/index.js
@@ -9,15 +9,60 @@ import { GraphQLServer } from "graphql-yoga"
 // type defination (Schema)
 const typeDefs = `
     type Query{
-        hello: String!
+       title: String!
+       price: Float!
+       releaseYear: Int
+       rating: Float
+       inSTock: Boolean!
+       me: User!
+       greeting(name: String): String!
+    }
+
+    type User{
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
     }
 `
 
 //Resolver
 const resolvers = {
     Query: {
-        hello(){
-            return 'this is my first query!'
+        title(){
+            return 'Product title'
+        },
+        price(){
+            return 110.88
+        },
+        releaseYear(){
+            return 2024
+        },
+        rating(){
+            return 8.3
+        },
+        inSTock(){
+            return true
+        },
+        me(){
+           return {
+            id: '123ABC',
+            name: 'Subham Deb',
+            email: 'subham.deb.1253@gmail.com'
+           } 
+        },
+        greeting(parent, argument, ctx, info){
+            return `Hello! ${argument.name}`
         }
     }
 }
+
+let server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(()=>{
+    console.log('the server is running up!')
+})
+
